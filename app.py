ba7957f35ecae509454b9ecaddc4c866f913b3c0
venugopal1902 <76394287+venugopal1902@users.noreply.github.com>
2022-02-23 15:31:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pywebio import STATIC_PATH
 from flask import Flask, send_from_directory
 from pywebio.input import *
 from pywebio.output import *
+from pywebio.output import put_text 
 import argparse
 from pywebio import start_server
 
@@ -13,41 +14,60 @@ app = Flask(__name__)
 
 
 def predict():
-    Year = input("Enter the Year in which year you have bought car：", type=NUMBER)
-    Year = 2021 - Year
-    Present_Price = input("Enter the Present Price(in LAKHS)", type=FLOAT)
-    Kms_Driven = input("Enter the distance it has travelled(in KMS)：", type=FLOAT)
+    Year = input("Enter the Year in which year you have bought car：", type=NUMBER,name=f's{1}')
     
-    Owner = input("Enter the number of owners who have previously owned it", type=NUMBER)
-    Fuel_Type = select('What is the Fuel Type', ['Petrol', 'Diesel','CNG'])
-    if (Fuel_Type == 'Petrol'):
-        Fuel_Type = 2
+    Present_Price = input("Enter the Present Price(in LAKHS)", type=FLOAT,name=f's{2}')
+    Kms_Driven = input("Enter the distance it has travelled(in KMS)：", type=FLOAT,name=f's{3}')
+    
+    Owner = input("Enter the number of owners who have previously owned it", type=NUMBER,name=f's{4}')
+    Fuel_Type = select('What is the Fuel Type', ['Petrol', 'Diesel','CNG'],name=f's{5}')
+    
+ 
+    Seller_Type = select('Are you a dealer or an individual', ['Dealer', 'Individual'],name=f's{6}')
+    Transmission = select('Transmission Type', ['Manual Car', 'Automatic Car'],name=f's{7}')
+    d = [Year,Present_Price, Kms_Driven, Fuel_Type, Seller_Type, Transmission, Owner]
+    d = input_group('basic info',d) 
+    print(d)
+    d= list(d.values())
+    print(d)
+    d[0] = 2021-d[0]
+    if (d[3] == 'Petrol'):
+        d[3] = 2
 
-    elif (Fuel_Type == 'Diesel'):
-        Fuel_Type = 1
+    elif (d[3] == 'Diesel'):
+        d[3] = 1
 
     else:
-        Fuel_Type = 3
-    Seller_Type = select('Are you a dealer or an individual', ['Dealer', 'Individual'])
-    if (Seller_Type == 'Individual'):
-        Seller_Type = 1
+        d[3] = 3
+    
+    if (d[4] == 'Individual'):
+        d[4] = 1
 
     else:
-        Seller_Type = 0
-    Transmission = select('Transmission Type', ['Manual Car', 'Automatic Car'])
-    if (Transmission == 'Manual Car'):
-        Transmission = 0
+        d[4] = 0
+    
+    if (d[5] == 'Manual Car'):
+        d[5] = 0
     else:
-        Transmission = 1
+        d[5] = 1
+    
+    print(d)
+       
+    
 
-    prediction = model.predict([[Present_Price, Kms_Driven, Fuel_Type, Seller_Type, Transmission, Owner, Year]])
+    prediction = model.predict([d])
     output = round(prediction[0], 2)
 
     if output < 0:
-        put_text("Sorry You can't sell this Car")
+        put_text("<h2>Sorry You can't sell this Car</h2>")
 
     else:
-        put_text('You can sell this Car at price(inlakhs):',output)
+        put_html(f'<h2>You can sell this Car at price(inlakhs): {output}</h2>')
+                 
+                
+                
+    put_html('<a href="/" style="background-color:blue;margin-left:350px;color:white;;padding :8px;border-radius:5px;font-size:30px;text-decoration:none;box-shadow:0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">Home</a>')
+      
 
 
 
